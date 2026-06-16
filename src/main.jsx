f5fc30a0a9b82ded6ab31e547060b48feb94a18a
Ann-Getit <Ann-Getit+182119360+Ann-Getit@users.noreply.github.com>
2026-06-16 13:10:18 +0200

    browserrouter veranderd naar hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react' /*checkt fouten*/
 import { createRoot } from 'react-dom/client' /*verbindt react met de DOM van de browser. door de componenten om te zetten als echte DOM elementen*/
 
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 /*react-router-dom  zorgt ervoor dat je kunt navigeren tussen paginas zonder te relouden door route,routes, browserroute*/
 import App from './App.jsx'
 
@@ -13,8 +13,8 @@ import App from './App.jsx'
 /* ALS 404 github pages krijg vervang browserrouter t hashroute. hashrouter is /#home/ bijv en github kan dit deze route vinden*/
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> 
+    <HashRouter> 
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
