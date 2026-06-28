bd41c74ace65e0d12a10acc44939116326a1ed8e
Ann-Getit <Ann-Getit+182119360+Ann-Getit@users.noreply.github.com>
2026-06-28 18:30:12 +0200

    eerste deel grouplessons added

--- src/pages/Grouplessons.jsx
+++ src/pages/Grouplessons.jsx
@@ -8,8 +8,36 @@ const Grouplessons = () => {
     return (
         <>
 
-        <div>hello world!</div>
+             <div className="container1">
+        <div className="twinparent1">
+            <h1>Live groupclasses and video-workouts</h1>
+            <p className="memberP"><strong>Choose classes that suites you better</strong></p>
+            <p className="memberP2">Train your way with live and virtual classes. Join sessions at the club or stream workouts through the app. From dancing to high-intensity, there is always a suitable choice for you. Always included in your membership.
+            </p>
 
+            <div className="buttonparent">
+            <button>Join us !</button>
+            </div>
+
+        </div>
+
+        <div className="parent1">
+           <img src="./pics/lessons-pics-unsplash.jpg" className="image_member"></img>
+  </div>
+        </div>
+
+
+        <div className="boxstroke22">
+        <div className="secondstroke">
+            <h2 className="positivetextt">
+                Find your perfect class
+            </h2>
+            <p>Go all the way with an energizing session. Relax with yoga or choose something in between. Join live at the club or stream on demand. New workouts every week.</p>
+        </div>
+        </div>
+{/*<div className="breedte">
+  {window.innerWidth}px
+</div>*/}
 
         
         </>
